1f0f45f39ba9174202455dcb03adc3c6c0a4ae5b
Tito Joctan <perfect.altruistic@egmail.com>
2024-03-10 18:00:12 +0300

    introduction: Part one

---
+++ java/introduction/partone/Message.java
@@ -0,0 +1,9 @@
+public class Message{
+    public static void main(String[] args){
+        // Variables and Data types in Java
+        String firstname = "John";
+        String lastname = "Doe";
+        String fullname = firstname + " " + lastname;
+        System.out.println("Your name is: " + fullname);
+    }
+}
