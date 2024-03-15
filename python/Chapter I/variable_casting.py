f2114b3cd8e746003ab2d40d8e94355eb076604f
Tito Joctan <perfect.altruistic@egmail.com>
2024-03-15 12:08:38 +0300

    Chapter I: Python Variable Casting

---
+++ python/Chapter I/variable_casting.py
@@ -0,0 +1,29 @@
+# Casting from string to integer
+str_num = "10"
+int_num = int(str_num)
+print(int_num)  # Output: 10
+
+# Casting from integer to string
+int_num = 5
+str_num = str(int_num)
+print(str_num)  # Output: "5"
+
+# Casting from float to integer
+float_num = 3.5
+int_num = int(float_num)
+print(int_num)  # Output: 3
+
+# Casting from integer to float
+int_num = 10
+float_num = float(int_num)
+print(float_num)  # Output: 10.0
+
+# Casting from integer to boolean
+int_value = 0
+bool_value = bool(int_value)
+print(bool_value)  # Output: False
+
+# Casting from boolean to integer
+bool_value = True
+int_value = int(bool_value)
+print(int_value)  # Output: 1
