069c022f6912885bf6c0b221141d0c4aa8a7b06e
Tito Joctan <perfect.altruistic@egmail.com>
2024-03-15 16:05:28 +0300

    Chapter II: Booleans Data Type

---
+++ python/Chapter II/Chapter IIa/Booleans.py
@@ -0,0 +1,25 @@
+# Boolean Data Type
+
+# Variables
+is_sunny = True
+is_raining = False
+
+# Explanation
+# Boolean data type represents truth values. It can have two possible values: True or False.
+# Booleans are commonly used in conditional statements and control flow to make decisions based on conditions.
+
+# Example
+# Checking weather conditions
+print("Is it sunny today?", is_sunny)
+print("Is it raining today?", is_raining)
+
+# Conditional statement
+if is_sunny:
+    print("Enjoy the sunny weather!")
+else:
+    print("Don't forget to carry an umbrella!")
+
+# In this example:
+# - We have two boolean variables is_sunny and is_raining initialized with True and False, respectively.
+# - We print the current weather conditions using these boolean variables.
+# - We use a conditional statement to check if it's sunny, and based on the result, we provide a corresponding message.
