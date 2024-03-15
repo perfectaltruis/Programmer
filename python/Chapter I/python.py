c87af77f1f16dfafba8a10cdc54917267dee355d
Tito Joctan <perfect.altruistic@egmail.com>
2024-03-15 12:09:04 +0300

    Chapter I: Python Overview

---
+++ python/Chapter I/python.py
@@ -0,0 +1,72 @@
+# What is Python?
+
+# Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+# It is used for:
+
+#  web development (server-side),
+# software development,
+# mathematics,
+# system scripting.
+
+# What can Python do?
+
+#   Python can be used on a server to create web applications.
+#  Python can be used alongside software to create workflows.
+#  Python can connect to database systems. It can also read and modify files.
+#  Python can be used to handle big data and perform complex mathematics.
+#  Python can be used for rapid prototyping, or for production-ready software development.
+
+# Why Python?
+
+# Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc.).
+# Python has a simple syntax similar to the English language.
+# Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
+# Python runs on an interpreter system, meaning that code can be executed as soon as it is written.
+# This means that prototyping can be very quick.
+# Python can be treated in a procedural way, an object-oriented way or a functional way.
+
+# Did You Know!! The most recent major version of Python is Python 3, which we shall be using in this tutorial.
+# However, Python 2, although not being updated with anything other than security updates, is still quite popular. In
+# this tutorial Python will be written in a text editor. It is possible to write Python in an Integrated Development
+# Environment, such as Thonny, Pycharm, Netbeans or Eclipse which are particularly useful when managing larger
+# collections of Python files.
+
+# Python Syntax compared to other programming languages
+
+# Python was designed for readability, and has some similarities to the English language with influence from
+# mathematics.
+
+
+# Python uses new lines to complete a command, as opposed to other programming languages which often use
+# semicolons or parentheses.
+
+
+# Python relies on indentation, using whitespace, to define scope; such as the scope of
+# loops, functions and classes. Other programming languages often use curly-brackets for this purpose but not as python.
+
+
+# To see if python is installed in you Machine please open your terminal and run the command below:
+# python  --version
+
+# if python is installed in your pc you could see something like this `Python 3.11.8`
+# try to run this command to inter the python command line interface `python`
+# And you should see something like
+
+# localhost@localhost:~/Programmer$ python
+# Python 3.11.8 (main, Feb  7 2024, 21:52:08) [GCC 13.2.0] on linux
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>>
+
+# from the line above you have python version 3.11.8 and this is implies that you have python installed on your PC and
+# So you can proceed to code.
+
+# If python is not installed on you PC please visit this site python.org to download and for more information how to
+# install python on any system. I will use Kali linux to create and code my project, remember python is for any OS.
+
+# Below is a simple example of how to print, output a message in python.
+# To run and see this in you screen open your terminal ad navigate to the folder where your main.py file is located and
+# type python main.py and then press (hit) enter. You should see the output of your messages.
+
+print("Hello World")
+print("Hey!, Please read the comment in his file first.")
