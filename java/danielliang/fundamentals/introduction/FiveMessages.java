eb22228c70e02296f5652d59914b30a77789c4ba
Tito Malima Joctan <perfect.altruistic@gmail.com>
2024-04-23 23:26:48 +0300

    1.2: display five messages

---
+++ java/danielliang/fundamentals/introduction/FiveMessages.java
@@ -0,0 +1,14 @@
+package danielliang.fundamentals.introduction;
+
+public class FiveMessages {
+
+    public static void main(String[] args) {
+        // five messages
+        System.out.println("\n");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
