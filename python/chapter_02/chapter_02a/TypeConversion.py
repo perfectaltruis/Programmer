38b5a137a1fdd4e724b980e936a4d750171ecda8
Tito Malima Joctan <perfect.altruistic@gmail.com>
2024-04-26 14:59:40 +0300

    chapter 02a: converting variables in python

---
+++ python/chapter_02/chapter_02a/TypeConversion.py
@@ -0,0 +1,26 @@
+# Type conversion example:
+# Python provides built-in functions to convert between different data types. Here's an example of type conversion:
+
+# Converting an integer to a float
+x = 10
+float_x = float(x)
+print(float_x)  # Output: 10.0
+print(type(float_x))  # Output: <class 'float'>
+
+# Converting a float to an integer
+y = 3.14
+int_y = int(y)
+print(int_y)  # Output: 3
+print(type(int_y))  # Output: <class 'int'>
+
+# Converting a string to an integer
+z = "20"
+int_z = int(z)
+print(int_z)  # Output: 20
+print(type(int_z))  # Output: <class 'int'>
+
+# Converting an integer to a string
+a = 30
+str_a = str(a)
+print(str_a)  # Output: '30'
+print(type(str_a))  # Output: <class 'str'>
