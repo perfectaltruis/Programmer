84f78bf30d1193f12f7b910454f27cc3464ced59
Tito Malima Joctan <perfect.altruistic@gmail.com>
2024-04-26 15:05:17 +0300

    chapter 02: Operators: comparison operators

---
+++ python/chapter_02/chapter_02a/Operators/Comparison.py
@@ -0,0 +1,41 @@
+# Comparison Operators
+
+# Variables
+first_number = 10
+second_number = 5
+
+# Equal to (==)
+print("Is first_number equal to second_number?", first_number == second_number)  # Output: False
+
+# Not equal to (!=)
+print("Is first_number not equal to second_number?", first_number != second_number)  # Output: True
+
+# Greater than (>)
+print("Is first_number greater than second_number?", first_number > second_number)  # Output: True
+
+# Less than (<)
+print("Is first_number less than second_number?", first_number < second_number)  # Output: False
+
+# Greater than or equal to (>=)
+print("Is first_number greater than or equal to second_number?", first_number >= second_number)  # Output: True
+
+# Less than or equal to (<=)
+print("Is first_number less than or equal to second_number?", first_number <= second_number)  # Output: False
+
+# In this script:
+#
+#     We have two variables first_number and second_number initialized with values 10 and 5, respectively.
+#     We demonstrate various comparison operators such as equal to (==), not equal to (!=), greater than (>),
+#     less than (<), greater than or equal to (>=), and less than or equal to (<=).
+#     Each comparison operation returns a boolean value (True or False) based on the comparison result.
+
+# Comparison operators are crucial in programming because they allow you to make decisions based on conditions.
+# Here are some reasons why comparison operators are important
+
+#     Control program flow in conditional statements like if, elif, and else.
+#     Define looping conditions in while and for loops.
+#     Filter data based on specific conditions.
+#     Enable sorting and ordering of data.
+#     Facilitate searching and indexing operations.
+#
+# In summary, comparison operators are essential for decision-making, data manipulation,and flow control in programming.
