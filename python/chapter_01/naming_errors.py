6da911098b65a81c76cb83c6838f6a80c5b67c9a
Tito Malima Joctan <perfect.altruistic@gmail.com>
2024-04-26 14:38:48 +0300

    chapter 01: naming errors in python

---
+++ python/chapter_01/naming_errors.py
@@ -0,0 +1,40 @@
+# ERRORS RELATED TO VARIABLE NAMES
+# Errors related to variable names in Python typically occur when the variable names do not adhere to the rules and
+# conventions of Python syntax. Here are some common errors related to variable names:
+#
+# SyntaxError: This error occurs if the variable name contains invalid characters or violates Python's naming rules.
+# For example:
+#
+# 123variable = 10  # Variable name starts with a digit
+#
+# NameError:
+# This error occurs when you try to access a variable that hasn't been defined or is misspelled. For example:
+#
+# print(my_variable)  # Variable 'my_variable' is not defined
+#
+# TypeError:
+# While not directly related to variable names, using inappropriate variable names might lead to
+# TypeErrors if the variable's intended purpose is misunderstood. For example:
+#
+# number = 'Hello'  # Variable name suggests a number, but it contains a string
+# number + 5  # This would raise a TypeError
+#
+# Reserved Keyword Conflict: If you accidentally use a reserved keyword as a variable name, it will result in syntax
+# errors or unexpected behavior. For example:
+#
+#
+# import = 10  # 'import' is a reserved keyword
+#
+# Incorrect Case Usage:
+# Python variable names are case-sensitive. Using different cases for the same variable name
+# can lead to confusion or errors. For example:
+#
+#     myVariable = 5
+#     print(MyVariable)  # This would result in a NameError
+#
+# To avoid these errors, ensure that your variable names adhere to Python's naming rules, avoid using reserved
+# keywords, use meaningful and descriptive names, and maintain consistent naming conventions throughout your code.
+
+print("Errors That relate and result due to variable names!")
+print("Please read this file carefully.")
+print("All related errors in python are described in it.")
