11fa1b9687f9c876db163be83c60c763375c6e75
Tito Malima Joctan <perfect.altruistic@gmail.com>
2024-04-26 15:06:34 +0300

    chapter 02: Operators: float operations

---
+++ python/chapter_02/chapter_02a/Operators/float_operations.py
@@ -0,0 +1,21 @@
+
+
+# Addition
+result_addition = 10.5 + 5.2
+print("Addition:", result_addition)
+
+# Subtraction
+result_subtraction = 10.5 - 5.2
+print("Subtraction:", result_subtraction)
+
+# Multiplication
+result_multiplication = 10.5 * 5.2
+print("Multiplication:", result_multiplication)
+
+# Division
+result_division = 10.5 / 5.2
+print("Division:", result_division)
+
+# Exponentiation
+result_exponentiation = 10.5 ** 2
+print("Exponentiation:", result_exponentiation)
