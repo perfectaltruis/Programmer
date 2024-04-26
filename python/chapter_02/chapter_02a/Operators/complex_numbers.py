3b6a9ae2c5cb6c76003247e436da6f918b014d77
Tito Malima Joctan <perfect.altruistic@gmail.com>
2024-04-26 15:06:07 +0300

    chapter 02: Operators

---
+++ python/chapter_02/chapter_02a/Operators/complex_numbers.py
@@ -0,0 +1,15 @@
+# Complex numbers
+complex_num1 = 3 + 4j
+complex_num2 = 2 - 1j
+
+# Addition
+result_addition = complex_num1 + complex_num2
+print("Addition of Complex Numbers:", result_addition)
+
+# Subtraction
+result_subtraction = complex_num1 - complex_num2
+print("Subtraction of Complex Numbers:", result_subtraction)
+
+# Multiplication
+result_multiplication = complex_num1 * complex_num2
+print("Multiplication of Complex Numbers:", result_multiplication)
