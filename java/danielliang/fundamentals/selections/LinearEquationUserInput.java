3dc8b5fd9a8f88bb0ac7a51d712b2bbaf66461d2
Tito Malima Joctan <perfect.altruistic@gmail.com>
2024-05-01 07:08:13 +0300

    3.3: algebra: solve 2 * 2 linear equations

--- java/danielliang/fundamentals/selections/LinearEquationUserInput.java
+++ java/danielliang/fundamentals/selections/LinearEquationUserInput.java
@@ -7,7 +7,8 @@ public class LinearEquationUserInput {
     public static void main(String[] args) {
         System.out.println("solve linear equaton of 2 * 2");
         
-        try (// scanner to store user input.
+        try (
+        // scanner to store user input.
         Scanner input = new Scanner(System.in)) {
             System.out.print("Enter value of a, b, c, d, e, f: ");
             double a = input.nextDouble();
